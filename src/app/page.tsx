6d604285a831edf58c0e29e8c963dc837a00415a
LilyFoNo <lilyfono21@gmail.com>
2025-03-03 18:12:29 -0800

    button component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import ButtonComp from "./components/ButtonComp";
 
 export default function Home() {
   return (
     <div className="">
-      <Image src={'/img/logo.svg'} alt="logo" width={100} height={100}/>
+      <Image src={"/img/logo.svg"} alt="logo" width={100} height={100} />
       <h1>UniCareer</h1>
+      <ButtonComp text="my button" IsWhite={false}/>
     </div>
   );
 }
