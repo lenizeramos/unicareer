afea28b7a47d3574e1f48e85ef3f68474e6beaee
LilyFoNo <lilyfono21@gmail.com>
2025-03-04 19:44:50 -0800

    some font styles and logo component

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,10 +1,17 @@
 const styles = {
   button:
-    "cursor-pointer rounded-sm px-4 py-2 font-semibold transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#5046feec] hover:text-gray-200 hover:shadow-lg",
-  heroHeadText: "",
-  heroSubText: "",
-  sectionHeadText: "",
-  sectionSubText: "",
+    "cursor-pointer rounded-sm px-4 py-2 font-semibold transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#5046feec] hover:text-gray-200 hover:shadow-lg font-shafarik",
+  heroHeadText: "font-bigShoulderStencil text-white font-bold",
+  heroHeadTextDark: "font-bigShoulderInline text-black",
+  titleHeroSize: "lg:text-[5rem] md:text-[3.125rem] text-[2.5rem]",
+  heroHeadSpan: "text-span",
+  heroSubText: "font-monomakh",
+  subtitleHeroSize:
+    "lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  sectionHeadText: "font-monomakh",
+  titleSectionSize: "md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+  sectionSubText: "font-shafarik",
+  subTitleSectionSize: "sm:text-[20px] text-[14px] ",
 };
 
 export { styles };
