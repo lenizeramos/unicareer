c56557d424e33758da88d080bd63a981c15e4e40
LilyFoNo <lilyfono21@gmail.com>
2025-03-05 17:35:07 -0800

    added fontsize in logo component

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,12 +3,13 @@ import { ILogo } from "../Types";
 
 const Logo = ({ logoSize, fontSize }: ILogo) => {
   const size = logoSize ? logoSize : 40;
+  const fontsize = fontSize ? fontSize : 'text-3xl'
   return (
     <>
       <div className={`flex items-center gap-2`}>
         <Image src={"/img/logo.svg"} alt="logo" width={size} height={size} />
         <h1
-          className={`font-bigShoulderStencil ${fontSize} logo-gradient font-medium`}
+          className={`font-bigShoulderStencil ${fontsize} logo-gradient font-medium`}
         >
           UniCareer
         </h1>
