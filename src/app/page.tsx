61048073754835d5235aa10e03faa55dd4136a07
LilyFoNo <lilyfono21@gmail.com>
2025-03-11 22:02:30 -0700

    dev merge into lili/cardsComponent/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { styles } from "./styles";
-
 export default function Home() {
   return (
     <>
-      <div
-        className={`${styles.titleHeroSize} ${styles.heroHeadTextDark} bg-landingDark`}
-      >
-        <p>Go to /test</p>
-      </div>
+      <div></div>
     </>
   );
 }
