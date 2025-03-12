4928f9e19a13c6971a5a59879cc71961abbe1c43
Jose Trueba <joshu2991@hotmail.com>
2025-03-11 22:44:21 -0700

    merge with test components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import type { Metadata } from "next";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+export const metadata: Metadata = {
+  title: "Unicareer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <div></div>
-    </>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="antialiased">
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
