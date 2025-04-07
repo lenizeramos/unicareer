3ba9b98da5a879ae87d3f6b6adb1ec2f18584f6f
EduardoCisneros <eduardo.cisnerosm@hotmail.com>
2025-04-07 17:51:17 -0600

    working on dashboard candidate page

--- src/app/(dashboard)/dashboard/candidate/page.tsx
+++ src/app/(dashboard)/dashboard/candidate/page.tsx
@@ -1,14 +1,18 @@
 import DashboardNavbar from "@/app/components/DashboardNavbar";
 import DashboardWelcome from "@/app/components/DashboardWelcome";
 import { styles } from "@/app/styles";
+import JobAppliedGraphic from "@/app/components/Cards/JobAppliedGraphic";
+
 
 export default function CandidatePage() {
+    
     return (
         <>
             <DashboardNavbar title="Dashboard" button={{ text: "Back to home page", IsWhite: true }} />
             <div className={styles.borderBottomLight}></div>
-            <DashboardWelcome greeting="Good Morning, Sam" message="Here is what’s happening with your job applications from July 19 - July 25." date="Jul 19 - Jul 25" />
+            <DashboardWelcome greeting="Good Morning, Sam" message="Here is what's happening with your job applications from July 19 - July 25." date="Jul 19 - Jul 25" />
+            <JobAppliedGraphic />
             
         </>
-    )
+    );
 }
