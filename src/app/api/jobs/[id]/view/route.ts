29570f91fe96d825bdd533f6b7fc6f7a66448bc6
Lenize Ramos <lenizemr@gmail.com>
2025-04-15 09:09:47 -0700

    on see details save views to db

---
+++ src/app/api/jobs/[id]/view/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse, NextRequest } from "next/server";
+import { getClerkUserId } from "@/utils/user";
+import { getUserByClerkId } from "@/Lib/usersService";
+import prisma from "@/Lib/prisma";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const jobId = params.id;
+    console.log("jobId*********", jobId);
+
+    if (!jobId) {
+        return new NextResponse("Job ID is required", { status: 400 });
+      }
+
+    const clerkUserId = await getClerkUserId();
+
+    if (!clerkUserId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const user = await getUserByClerkId(clerkUserId);
+
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+    if (!user.candidate) {
+      return new NextResponse("Only candidates can view jobs", { status: 403 });
+    }
+
+    
+
+    const jobExists = await prisma.job.findUnique({
+      where: { id: jobId },
+    });
+
+    if (!jobExists) {
+      return new NextResponse("Job not found", { status: 404 });
+    }
+
+    await prisma.jobView.upsert({
+      where: {
+        jobId_candidateId: {
+          jobId,
+          candidateId: user.candidate.id,
+        },
+      },
+      create: {
+        jobId,
+        candidateId: user.candidate.id,
+      },
+      update: {},
+    });
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error recording job view:", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
