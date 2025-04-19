12ccc439cb232e02d98899ea0c573a06cc015555
Jose Trueba <joshu2991@hotmail.com>
2025-04-19 15:50:14 -0700

    job slices updated

--- src/app/context/slices/jobSlices.ts
+++ src/app/context/slices/jobSlices.ts
@@ -11,7 +11,7 @@ const initialState: IJobsState = {
 export const fetchAllJobs = createAsyncThunk(
   "fetchAllJobs",
   async () => {
-    const response = await fetch("http://localhost:3000/api/get-jobs");
+    const response = await fetch("/api/get-jobs");
     if (!response.ok) {
       throw new Error("Failed to fetch jobs");
     }
