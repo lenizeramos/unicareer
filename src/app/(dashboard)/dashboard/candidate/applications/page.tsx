68a816f6651ae521c075687962d6bebadac72bb5
LilyFoNo <lilyfono21@gmail.com>
2025-04-21 22:36:30 -0700

    Update applications page

--- src/app/(dashboard)/dashboard/candidate/applications/page.tsx
+++ src/app/(dashboard)/dashboard/candidate/applications/page.tsx
@@ -124,47 +124,47 @@ export default function Application() {
           />
         </div>
       </div>
-      {data.length === 0 ? (
-        <SearchNotFound text="No applicantions found" />
-      ) : (
-        <div className="mt-3">
-          <div className="font-shafarik flex md:gap-20 sm:gap-15 gap-5 sm:text-lg text-[14px] border-b-[1px] border-gray-200 px-3 pt-3">
-            {statusTags.map((status, index) => {
-              return (
-                <p
-                  id={status.id}
-                  key={index}
-                  onClick={() => setActive(status.id)}
-                  className={`${
-                    active === status.id
-                      ? "text-black font-semibold border-b-2 border-[#5939c6] px-2"
-                      : "text-gray-500"
-                  } cursor-pointer`}
-                >
-                  {status.type}
-                </p>
-              );
-            })}
+      <div className="mt-3">
+        <div className="font-shafarik flex md:gap-20 sm:gap-15 gap-5 sm:text-lg text-[14px] border-b-[1px] border-gray-200 px-3 pt-3">
+          {statusTags.map((status, index) => {
+            return (
+              <p
+                id={status.id}
+                key={index}
+                onClick={() => setActive(status.id)}
+                className={`${
+                  active === status.id
+                    ? "text-black font-semibold border-b-2 border-[#5939c6] px-2"
+                    : "text-gray-500"
+                } cursor-pointer`}
+              >
+                {status.type}
+              </p>
+            );
+          })}
+        </div>
+        <div className="mt-10 px-3 flex justify-between">
+          <h2 className="font-semibold font-shafarik text-xl">
+            Applications History
+          </h2>
+          <div className="flex items-center border rounded-xl p-1 border-gray-400 w-fit h-fit">
+            <BsSearch color="#99a1af" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="font-shafarik px-3 w-[5rem] sm:text-md text-sm border-none outline-none"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
           </div>
-          <div className="mt-10 px-3 flex justify-between">
-            <h2 className="font-semibold font-shafarik text-xl">
-              Applications History
-            </h2>
-            <div className="flex items-center border rounded-xl p-1 border-gray-400 w-fit h-fit">
-              <BsSearch color="#99a1af" />
-              <input
-                type="text"
-                placeholder="Search"
-                className="font-shafarik px-3 w-[5rem] sm:text-md text-sm border-none outline-none"
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-            </div>
-          </div>
+        </div>
+        {data.length === 0 ? (
+          <SearchNotFound text="No applicantions found" />
+        ) : (
           <div>
             <SummaryTable columnNames={columnNames} data={data} />
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 }
