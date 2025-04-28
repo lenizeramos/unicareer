279b23469b51fca9ae82373e8956d354f2e1a093
Lenize Ramos <lenizemr@gmail.com>
2025-04-28 08:21:23 -0700

    fix candidate profile page

--- src/app/(dashboard)/dashboard/candidate/profile/page.tsx
+++ src/app/(dashboard)/dashboard/candidate/profile/page.tsx
@@ -28,18 +28,16 @@ const ProfilePage = () => {
         title="My Profile"
         button={{ text: "Back to dashboard", IsWhite: true }}
       />
-      <div className="flex flex-col sm:flex-col md:flex-row min-h-screen">
-        <div className="flex-1 p-4 md:p-6">
-          <div className="bg-white shadow-md rounded-2xl p-4 md:p-6 mb-4 md:mb-6 relative">
-            <div className="flex justify-between">
-              <div
-                className="absolute top-0 left-0 w-full md:h-24 h:18 bg-cover bg-center rounded-t-2xl"
-                style={{
-                  backgroundImage: "url('/img/background-candidate.png')",
-                }}
-              ></div>
-            </div>
-            <div className="flex items-center space-x-4 relative z-10 mt-12">
+      <div className="flex flex-col md:flex-row gap-4 font-shafarik">
+        <div className="flex flex-col md:w-2/3 gap-6 md:p-6">
+          <div className="p-4 md:p-6 relative shadow-xl rounded-2xl">
+            <div
+              className="absolute top-0 left-0 w-full md:h-24 h-18 bg-cover bg-center rounded-t-2xl"
+              style={{
+                backgroundImage: "url('/img/background-candidate.png')",
+              }}
+            />
+            <div className="flex items-center md:gap-4 gap-1 relative z-10 mt-8">
               <FileDisplay
                 modelName="userProfileImage"
                 userId={candidate?.user?.id || ""}
@@ -48,14 +46,17 @@ const ProfilePage = () => {
                 className="profile-image-style overflow-hidden"
                 fallbackImage={candidate?.user?.photo || ""}
               />
-              <div className="flex-1">
-                <h3 className="text-xl font-bold">{`${candidate?.firstName} ${candidate?.lastName}`}</h3>
-                <span className="mt-2 px-3 py-1 bg-green-100 text-green-700 text-xs rounded-full">
-                  {`${candidate?.user?.city}, ${candidate?.user?.province} `}
-                </span>
+              <div className="flex flex-col flex-1">
+                <h3 className="md:text-3xl text-lg font-bold font-monomakh">{`${candidate?.firstName} ${candidate?.lastName}`}</h3>
+                {candidate?.user?.city && (
+                  <span className="px-2 py-1 bg-gray-100 text-[#49286b] md:text-md text-[10px] rounded-full font-shafarik w-fit">
+                    {candidate.user.city && `${candidate.user.city}`}{" "}
+                    {candidate.user.province && `, ${candidate.user.province}`}
+                  </span>
+                )}
               </div>
 
-              <div className="absolute top-2 right-2">
+              <div className="">
                 <ButtonComp
                   text="Edit Profile"
                   IsWhite={true}
@@ -68,36 +69,30 @@ const ProfilePage = () => {
           </div>
 
           <div className="p-6 w-full border border-gray-200">
-            <div className="flex justify-between">
-              <h4 className={styles.sectionHeadText}>About Me</h4>
-            </div>
-            <p className="text-gray-700">
+            <h4 className={`${styles.sectionHeadText}`}>About Me</h4>
+            <p className="text-gray-700 ">
               {candidate?.bio || "No bio available"}
             </p>
           </div>
 
           <div className="p-6 w-full border border-gray-200">
-            <div className="flex justify-between">
-              <h4 className={`${styles.sectionHeadText}`}>Experiences</h4>
-            </div>
+            <h4 className={`${styles.sectionHeadText}`}>Experiences</h4>
             <div className="mt-4 space-y-4">
               {candidate?.workExperience?.map((exp) => (
-                <div key={exp.id} className="flex items-start gap-4">
-                  <div>
-                    <h5 className="font-bold">{exp.position}</h5>
-                    <p className="text-sm text-gray-500">
+                <div key={exp.id} className="">
+                  <div className="space-y-2">
+                    <h5 className="font-bold md:text-xl">{exp.position}</h5>
+                    <p className="text-sm text-gray-800">
                       {exp.company} - {exp.country}
                     </p>
                     <p className="text-sm text-gray-500">
-                      {exp.startDate
-                        ? new Date(exp.startDate).toLocaleDateString()
-                        : ""}{" "}
+                      {exp.startDate &&
+                        new Date(exp.startDate).toLocaleDateString()}{" "}
                       -{" "}
-                      {exp.current
+                      {!exp.current
                         ? "Present"
-                        : exp.endDate
-                        ? new Date(exp.endDate).toLocaleDateString()
-                        : ""}
+                        : exp.endDate &&
+                          new Date(exp.endDate).toLocaleDateString()}
                     </p>
                     <p className="text-gray-700">{exp.description}</p>
                   </div>
@@ -107,9 +102,7 @@ const ProfilePage = () => {
           </div>
 
           <div className="p-6 w-full border border-gray-200">
-            <div className="flex justify-between">
-              <h4 className={`${styles.sectionHeadText}`}>Education</h4>
-            </div>
+            <h4 className={`${styles.sectionHeadText}`}>Education</h4>
             <div className="mt-4 space-y-4">
               {candidate?.education?.map((edu) => (
                 <div key={edu.id} className="flex items-start gap-4">
@@ -162,7 +155,8 @@ const ProfilePage = () => {
             </div>
           </div>
         </div>
-        <aside className="md:w-1/3 w-fit bg-gray-50 space-y-5 md:mt-6 p-6">
+
+        <aside className=" md:w-1/3 w-fit bg-gray-50 space-y-5 md:mt-6 p-6">
           <div className="overflow-hidden text-ellipsis">
             <h4 className={`${styles.sectionHeadText} md:text-center mb-2`}>
               Additional Details
@@ -173,8 +167,8 @@ const ProfilePage = () => {
             </p>
             <div className="text-sm">
               <div className="flex gap-2 items-center">
-                <FaGlobe className="text-gray-600" />
-                <h1 className="text-gray-800 font-semibold">Website </h1>
+                <FaGlobe className="text-gray-600"/>
+                <h4 className="text-gray-800 font-semibold">Website:</h4>{" "}
               </div>
               <a
                 className=" text-blue-600 hover:underline cursor-pointer"
@@ -190,7 +184,7 @@ const ProfilePage = () => {
             <div className="text-sm gap-2">
               <div className="flex gap-2 items-center">
                 <FaTwitter className="text-gray-600" />
-                <h1 className="text-gray-800 font-semibold">Twitter </h1>
+                <h4 className="text-gray-800 font-semibold">Twitter:</h4>{" "}
               </div>
               <a
                 href={`${candidate?.user?.twitter}`}
@@ -202,7 +196,7 @@ const ProfilePage = () => {
             <div className="text-sm">
               <div className=" flex gap-2 items-center">
                 <FaLinkedinIn className="text-gray-600" />
-                <h1 className="text-gray-800 font-semibold">Linkedin</h1>
+                <h4 className="text-gray-800 font-semibold">Linkedin:</h4>{" "}
               </div>
               <a
                 href={`${candidate?.user?.linkedIn}`}
